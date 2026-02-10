d08ea14ecebe17662b5f6ffaad8cafa0b7b267c9
MohamedBenalou <mohameedx31x@gmail.com>
2026-02-10 15:29:37 +0100

    Update encryption.php

--- encryption.php
+++ encryption.php
@@ -1,26 +1,13 @@
 <?php
 $str = '';
-function encryption($text)
+function str_len($text)
 {
-    $rev = '';
-    $coding = '';
+   
     $text_len = 0;
     while (isset($text[$text_len])) {
         $text_len++;
     }
-    $i = $text_len - 1;
-    $rev_len = 0;
-    while ($i >= 0) {
-        $rev .= $text[$i];
-        $rev_len++;
-        $i--;
-    }
-    $j = 0;
-    while ($j < $rev_len) {
-        $add = ord($rev[$j]);
-        $coding .= chr($add + 22);
-        $j++;
-    }
-    return  $coding;
+   
+    return  $text_len;
 }
-echo encryption($str);
+echo str_len($str);
